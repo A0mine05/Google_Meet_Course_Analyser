cb7a17af4e77ac5565ef7bf612b61faa6ecad373
nanouchaa <84351145+nanouchaa@users.noreply.github.com>
2021-05-19 20:35:43 +0000

    Add files via upload

--- bdd.sql
+++ bdd.sql
@@ -1,11 +1,16 @@
-CREATE DATABASE googleMeet;
-USE googleMeet;
 
+DROP TABLE IF EXISTS ETUDIANT;
+DROP TABLE IF EXISTS ENSEIGNANT;
+DROP TABLE IF EXISTS CLASSE;
+DROP TABLE IF EXISTS MATIERE;
+DROP TABLE IF EXISTS SCEANCE;
+DROP TABLE IF EXISTS LISTE_PRESENCE;
+DROP TABLE IF EXISTS REPONSE;
+DROP TABLE IF EXISTS FORMULAIRE;
 
-
-CREATE TABLE IF NOT EXIST ETUDIANT(
-    idEtudiant int primary key,
-    idParticipant, 
+CREATE TABLE ETUDIANT(
+    idEtudiant int primary key NOT NULL AUTO_INCREMENT,
+    idParticipant int, 
     nomUtilisateur varchar(50),
     motDePasse varchar(50),
     email varchar(50),
@@ -14,10 +19,10 @@ CREATE TABLE IF NOT EXIST ETUDIANT(
     typeTerminal varchar(50)
 );
 
-CREATE TABLE IF NOT EXIST ENSEIGNANT
+CREATE TABLE ENSEIGNANT
 (
-    matricule int primary key,
-    idAdmin int pimary key, 
+    idAdmin int pRimary key NOT NULL AUTO_INCREMENT, 
+    matricule int ,
     nomUtilisateur varchar(50),
     motDePasse varchar(50),
     email varchar(50),
@@ -26,22 +31,24 @@ CREATE TABLE IF NOT EXIST ENSEIGNANT
     typeTerminal varchar(50)
 );
 
-CREATE TABLE IF NOT EXIST CLASSE
+CREATE TABLE CLASSE
 (
-    idClasse int primary key,
+    idClasse int primary key NOT NULL AUTO_INCREMENT,
     idParticipant varchar(50),
+    effectif int,
+    nomClasse varchar(50)
    
 );
 
-CREATE TABLE IF NOT EXIST MATIERE
+CREATE TABLE MATIERE
 (
-    idMatiere int primary key,
+    idMatiere int primary key NOT NULL AUTO_INCREMENT,
     intitule varchar(20)
 );
 
-CREATE TABLE IF NOT EXIST SCEANCE
+CREATE TABLE SCEANCE
 (
-    idSeance int primary key,
+    idSeance int primary key NOT NULL AUTO_INCREMENT,
     heureDebut date,
     heureFin date,
     nbrePartageEcran int,
@@ -51,9 +58,42 @@ CREATE TABLE IF NOT EXIST SCEANCE
     nbreDeconnexion int
 );
 
-CREATE TABLE IF NOT EXIST LISTE_PRESENCE
+CREATE TABLE LISTE_PRESENCE
 (
-    idListePresence int primary key,
+    idListePresence int primary key NOT NULL AUTO_INCREMENT,
     idParticipant int,
     nomUtilisateur varchar(50)
+);
+
+CREATE TABLE REPONSE
+(
+    reponse0 varchar(50),
+    reponse1 varchar(50),
+    reponse2 varchar(50),
+    reponse3 varchar(50),
+    reponse4 varchar(50),
+    reponse5 varchar(50), 
+    reponse6 varchar(50), 
+    reponse7 varchar(50), 
+    reponse8 varchar(50), 
+    reponse9 varchar(50), 
+    reponse10 varchar(50), 
+    reponse11 varchar(50), 
+    reponse12 varchar(50), 
+    reponse13 varchar(50), 
+    reponse14 varchar(50), 
+    reponse15 varchar(50), 
+    reponse16 varchar(50), 
+    reponse17 varchar(50), 
+    reponse18 varchar(50), 
+    reponse19 varchar(50),
+    reponse20 varchar(50)
+);
+
+CREATE TABLE FORMULAIRE
+(
+    idFormulaire int primary key NOT NULL AUTO_INCREMENT,
+    heureF time,
+    dateF datetime not null,
+    score int
 );
